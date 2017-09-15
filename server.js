9efdc4080cbbd1ed573f35a981cc1b299c44d707
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-09-15 09:29:45 -0400

    modify serve to run on port 3001 for development

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const mongoose = require('mongoose');
 const Task = require('./api/models/todoListModel');
 const User = require('./api/models/user');
